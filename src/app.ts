9a636587b16a0b65302addf679e60e4b45fe0de1
krouspy <krouspyy@gmail.com>
2022-03-03 07:57:42 +0100

    🚧 wip

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,72 @@
 import { ethers } from 'ethers';
+import { ContractType } from '@prisma/client';
 import { prisma } from '~/modules/db';
-import { getERC721, isERC721 } from '~/modules/contracts';
+import {
+  isERC721,
+  getERC721,
+  getERC721NameAndSymbol,
+  getContractDeploymentsInBlock,
+  getBlockLogs,
+} from '~/modules/contracts';
 import { ALCHEMY_API_KEY } from './constants';
-import { TxResponse } from '~/types';
+import { TxResponse } from './types';
 
 const provider = new ethers.providers.AlchemyProvider('goerli', ALCHEMY_API_KEY);
 
-const getContractMetadata = async (address: string) => {
-  const contract = getERC721(address, provider);
-  const [name, symbol, isERC721, isERC1155] = await Promise.all([
-    contract.name(),
-    contract.symbol(),
-    contract.supportsInterface('0x80ac58cd'),
-    contract.supportsInterface('0xd9b67a26'),
-  ]);
-  return { name, symbol, isERC721, isERC1155 };
-};
-
-async function main() {
-  const blockNumber = 6439505;
-  const { hash, number, timestamp, transactions } = await provider.getBlockWithTransactions(blockNumber);
-  const contracts: TxResponse[] = transactions.filter((tx: TxResponse) => Boolean(tx.creates));
-
-  console.log(`found ${contracts.length} contract deployments`);
-
-  if (contracts.length === 0) {
-    return;
-  }
-
+async function registerContracts(contracts: TxResponse[]) {
   for (const contract of contracts) {
-    if (await isERC721(contract.creates, provider)) {
-      console.log(`found an ERC721 ${contract.creates}`);
+    const contractAddress = contract.creates;
+    if (await isERC721(contractAddress, provider)) {
+      const erc721 = getERC721(contractAddress, provider);
+      const { name, symbol } = await getERC721NameAndSymbol(erc721);
+      console.log(`registering new ERC721 ${contractAddress} with name: ${name} and symbol: ${symbol}`);
+      /* await prisma.contract.create({
+        data: {
+          address: contractAddress,
+          deployer: contract.from,
+          name,
+          symbol,
+          contractType: ContractType.ERC721,
+          blockNumber,
+        },
+      }); */
     }
   }
 }
 
-main();
+async function main() {
+  // const blockNumber = 6439505;
+  const blockNumber = 6458052;
+  const block = await provider.getBlockWithTransactions(blockNumber);
+  const contracts = getContractDeploymentsInBlock(block);
+  console.log(`found ${contracts.length} contract deployments in block ${block.number}`);
+
+  if (contracts.length === 0) {
+    // await registerContracts(contracts);
+  }
+
+  const blockLogs = await getBlockLogs(block, provider);
+
+  blockLogs.forEach((transactionLogs) => {
+    transactionLogs.forEach((log) => {
+      switch (log.name) {
+        case 'Transfer':
+          console.log(log);
+          break;
+        case 'Approval':
+          console.log(log);
+          break;
+        case 'ApprovalForAll':
+          console.log(log);
+          break;
+        default:
+          console.log(`Event ${log.name} not supported`);
+          break;
+      }
+    });
+  });
+
+  // console.log(blockLogs);
+}
+
+main().catch(console.error);
--- src/modules/contracts/index.ts
+++ src/modules/contracts/index.ts
@@ -1,4 +1,7 @@
-import { AlchemyProvider } from '@ethersproject/providers';
+import { ethers } from 'ethers';
+import type { AlchemyProvider } from '@ethersproject/providers';
+import type { BlockWithTransactions } from '@ethersproject/abstract-provider';
+import type { TxResponse } from '~/types';
 import { Contract, ContractInterface } from '@ethersproject/contracts';
 import { AddressZero } from '@ethersproject/constants';
 import { getAddress } from '@ethersproject/address';
@@ -28,7 +31,45 @@ export function getERC721(address: string, provider: AlchemyProvider) {
   return getContract<ERC721>(address, ERC721Abi, provider);
 }
 
+export async function getERC721NameAndSymbol(contract: ERC721) {
+  const [name, symbol] = await Promise.all([contract.name(), contract.symbol()]);
+  return { name, symbol };
+}
+
 export async function isERC721(address: string, provider: AlchemyProvider) {
   const contract = getERC721(address, provider);
   return contract.supportsInterface('0x80ac58cd');
+}
+
+export async function isERC1155(address: string, provider: AlchemyProvider) {
+  const contract = getERC721(address, provider);
+  return contract.supportsInterface('0xd9b67a26');
+}
+
+export async function getLogsFromTransaction(transactionHash: string, provider: AlchemyProvider) {
+  const iface = new ethers.utils.Interface(ERC721Abi);
+  const receipt = await provider.getTransactionReceipt(transactionHash);
+  // try to parse using ERC721 abi
+  const logs = receipt.logs.map((log) => {
+    try {
+      return iface.parseLog(log);
+    } catch {
+      return null;
+    }
+  });
+  // remove 'null' elements
+  return logs.filter((log) => log);
+}
+
+export function getContractDeploymentsInBlock(block: BlockWithTransactions) {
+  return block.transactions.filter((tx: TxResponse) => Boolean(tx.creates));
+}
+
+// get logs of non-contract deployments transactions
+export async function getBlockLogs(block: BlockWithTransactions, provider: AlchemyProvider) {
+  // non-contract deployments
+  const transactions: TxResponse[] = block.transactions.filter((tx: TxResponse) => !Boolean(tx.creates));
+  const logsPerTransaction = await Promise.all(transactions.map(({ hash }) => getLogsFromTransaction(hash, provider)));
+  // remove empty elements
+  return logsPerTransaction.filter((transactionLogs) => transactionLogs.length);
 }
